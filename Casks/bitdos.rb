c0eab10c82dbadbe11eeae5e2cbba7751fdd5904
fazibear <fazibear@gmail.com>
2026-09-20 23:21:57 +0200

    Add bitdos cask (#225)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ Casks/bitdos.rb
@@ -0,0 +1,10 @@
+cask "bitdos" do
+  version "1.2"
+  sha256 :no_check
+  url "https://github.com/astriiddev/BitDOS-VST/releases/download/1.2/BitDOS_macOS.pkg"
+  name "BitDOS"
+  desc "Free audio plugin"
+  homepage "https://github.com/astriiddev/BitDOS-VST"
+  depends_on :macos
+  pkg "BitDOS_macOS.pkg"
+end
