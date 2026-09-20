f809466b044a56624cab50ae23a54860988ceeaf
fazibear <fazibear@gmail.com>
2026-09-20 23:20:10 +0200

    Add VOPM Yamaha YM2151 emulation casks (#215)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ Casks/vopm-yamaha-ym2151-emulation-vst.rb
@@ -0,0 +1,10 @@
+cask "vopm-yamaha-ym2151-emulation-vst" do
+  version "latest"
+  sha256 :no_check
+  url "https://alt1.plugins4free.com/get_plug/VOPM_MacVST.zip"
+  name "VOPM Yamaha YM2151 emulation"
+  desc "VOPM Yamaha YM2151 emulation"
+  homepage "https://plugins4free.com/plugin/1066"
+  depends_on :macos
+  artifact "VOPM_MacVST", target: "#{Dir.home}/Library/Audio/Plug-Ins/VST"
+end
