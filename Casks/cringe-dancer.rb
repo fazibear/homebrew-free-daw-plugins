7e74b85d743c644b99b0422ce65199a1bcd83f0b
fazibear <fazibear@gmail.com>
2026-09-20 21:14:00 +0200

    Add cringe-dancer cask (#104)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ Casks/cringe-dancer.rb
@@ -0,0 +1,10 @@
+cask "cringe-dancer" do
+  version "1.0.0"
+  sha256 "88d81ee77d4680608f1b671785460543fc04e19638b289f44cd8c86aa926adc2"
+  url "https://github.com/alexlarichev/cringe-dancer-releases/releases/download/v1.0.0/CringeDancer-1.0.0-mac.dmg"
+  name "Cringe Dancer Releases"
+  desc "Free audio plugin"
+  homepage "https://github.com/alexlarichev/cringe-dancer-releases"
+  depends_on :macos
+  dmg "CringeDancer-1.0.0-mac.dmg"
+end
