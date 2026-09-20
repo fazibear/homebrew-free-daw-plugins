2d2657107241c099d51a57c8ce14efdeb81b71f3
Michał Kalbarczyk <fazibear@gmail.com>
2026-09-20 19:50:50 +0200

    add gha

--- Casks/rverse.rb
+++ Casks/rverse.rb
@@ -1,7 +1,7 @@
 cask "rverse" do
   version "1.1.0"
   sha256 :no_check
-  url "https://github.com/SamuFL/rverse/releases/latest/download/RVRSE-#{version}-macOS.pkg"
+  url "https://github.com/SamuFL/rverse/releases/download/v#{version}/RVRSE-#{version}-macOS.pkg"
   name "RVRSE"
   desc "Reverse-reverb riser and hit designer audio plugin"
   homepage "https://github.com/SamuFL/rverse"
