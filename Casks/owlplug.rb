b837308c992bde681b594e7cbf268c04b03bd114
fazibear <fazibear@gmail.com>
2026-09-20 23:09:52 +0200

    Add owlplug cask (#196)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ Casks/owlplug.rb
@@ -0,0 +1,10 @@
+cask "owlplug" do
+  version "2.0.1"
+  sha256 "100513f647f87a04226eb28292fa10da74d8ad460429277fded29499035acce4"
+  url "https://github.com/DropSnorz/OwlPlug/releases/download/2.0.1/OwlPlug-2.0.1-osx-arm64.dmg"
+  name "OwlPlug"
+  desc "Free audio plugin"
+  homepage "https://github.com/DropSnorz/OwlPlug"
+  depends_on :macos
+  dmg "OwlPlug-2.0.1-osx-arm64.dmg"
+end
