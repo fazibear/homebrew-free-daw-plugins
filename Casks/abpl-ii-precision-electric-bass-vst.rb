419d30a383594bd7d1b0028438c1a53985610404
fazibear <fazibear@gmail.com>
2026-09-20 23:20:47 +0200

    Add ABPL II Precision electric bass casks (#220)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ Casks/abpl-ii-precision-electric-bass-vst.rb
@@ -0,0 +1,10 @@
+cask "abpl-ii-precision-electric-bass-vst" do
+  version "latest"
+  sha256 :no_check
+  url "https://alt1.plugins4free.com/get_plug/ABPL_2_3_1_Complete_Installer.dmg"
+  name "ABPL II Precision electric bass"
+  desc "ABPL II Precision electric bass"
+  homepage "https://plugins4free.com/plugin/2505"
+  depends_on :macos
+  dmg "ABPL_2_3_1_Complete_Installer.dmg"
+end
