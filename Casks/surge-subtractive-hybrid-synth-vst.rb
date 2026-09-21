4536851bb542d07d3a12b976fbef78cf44a3e0a8
fazibear <fazibear@gmail.com>
2026-09-21 09:16:30 +0200

    Add Surge Subtractive hybrid synth casks (#339)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ Casks/surge-subtractive-hybrid-synth-vst.rb
@@ -0,0 +1,10 @@
+cask "surge-subtractive-hybrid-synth-vst" do
+  version "latest"
+  sha256 :no_check
+  url "https://alt1.plugins4free.com/get_plug/Surge.dmg"
+  name "Surge Subtractive hybrid synth"
+  desc "Surge Subtractive hybrid synth"
+  homepage "https://plugins4free.com/plugin/2931"
+  depends_on :macos
+  dmg "Surge.dmg"
+end
