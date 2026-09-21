32e27516f0364b79eaadc9520f1bc02cae4e148e
fazibear <fazibear@gmail.com>
2026-09-21 21:47:02 +0200

    Add Mobius casks (#429)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ Casks/mobius-au.rb
@@ -0,0 +1,10 @@
+cask "mobius-au" do
+  version "latest"
+  sha256 :no_check
+  url "https://alt1.plugins4free.com/get_plug/Mobius.pkg"
+  name "Mobius"
+  desc "Mobius is a real-time 8 part looper / slicer designed for live creation of audio loops."
+  homepage "https://plugins4free.com/plugin/3330"
+  depends_on :macos
+  pkg "Mobius.pkg"
+end
