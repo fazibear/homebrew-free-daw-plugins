365dbd9e194a8d3a0e0d8455c65309ffd6000572
fazibear <fazibear@gmail.com>
2026-09-21 03:44:57 +0200

    Add MPS Musical parameter synth casks (#328)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ Casks/mps-musical-parameter-synth-vst.rb
@@ -0,0 +1,10 @@
+cask "mps-musical-parameter-synth-vst" do
+  version "latest"
+  sha256 :no_check
+  url "https://alt1.plugins4free.com/get_plug/mps_1_2_1_mac.pkg"
+  name "MPS Musical parameter synth"
+  desc "MPS Musical parameter synth"
+  homepage "https://plugins4free.com/plugin/2920"
+  depends_on :macos
+  pkg "mps_1_2_1_mac.pkg"
+end
