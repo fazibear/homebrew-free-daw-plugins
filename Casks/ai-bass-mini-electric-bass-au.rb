d44bb26dbbc38beaa4db74dc49175a3eb13765ce
fazibear <fazibear@gmail.com>
2026-09-21 03:45:07 +0200

    Add AI Bass Mini Electric bass casks (#330)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ Casks/ai-bass-mini-electric-bass-au.rb
@@ -0,0 +1,10 @@
+cask "ai-bass-mini-electric-bass-au" do
+  version "latest"
+  sha256 :no_check
+  url "https://alt1.plugins4free.com/get_plug/AI_Bass_Mini-Mac.zip"
+  name "AI Bass Mini Electric bass"
+  desc "AI Bass Mini Electric bass"
+  homepage "https://plugins4free.com/plugin/3940"
+  depends_on :macos
+  artifact "AI_Bass_Mini-Mac", target: "#{Dir.home}/Library/Audio/Plug-Ins/Components"
+end
