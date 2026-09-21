6a3b0368f31a145aa12c0292dc73a7be178dac21
fazibear <fazibear@gmail.com>
2026-09-21 21:28:41 +0200

    Add intersect cask (#423)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ Casks/intersect.rb
@@ -0,0 +1,10 @@
+cask "intersect" do
+  version "0.15.4"
+  sha256 "c7049481a9bd24e02d1a3c7235a5d42bc36ad5d2f92a6a93c3690282856b1980"
+  url "https://github.com/tucktuckg00se/INTERSECT/releases/download/v0.15.4/INTERSECT-v0.15.4-macOS-arm64.zip"
+  name "INTERSECT"
+  desc "Free audio plugin"
+  homepage "https://github.com/tucktuckg00se/INTERSECT"
+  depends_on :macos
+  artifact "INTERSECT.vst3", target: "#{Dir.home}/Library/Audio/Plug-Ins/VST3"
+end
