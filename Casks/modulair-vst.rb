e11145ac8835c8d707ffce22e607f090a8c2bbb6
fazibear <fazibear@gmail.com>
2026-09-23 10:00:01 +0200

    Add ModulAir casks (#502)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ Casks/modulair-vst.rb
@@ -0,0 +1,10 @@
+cask "modulair-vst" do
+  version "latest"
+  sha256 :no_check
+  url "https://alt1.plugins4free.com/get_plug/modulair_1_3_2_mac.pkg"
+  name "ModulAir"
+  desc "ModulAir is a polyphonic modular synthesizer ."
+  homepage "https://plugins4free.com/plugin/2914"
+  depends_on :macos
+  pkg "modulair_1_3_2_mac.pkg"
+end
