e89e5e978d9a9d6f4b652e569dd45894b1c2bd83
fazibear <fazibear@gmail.com>
2026-09-24 14:21:26 +0200

    Add Helm casks (#525)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ Casks/helm-vst.rb
@@ -0,0 +1,10 @@
+cask "helm-vst" do
+  version "latest"
+  sha256 :no_check
+  url "https://alt1.plugins4free.com/get_plug/Helm.pkg"
+  name "Helm"
+  desc "Helm is a polyphonic semi-modular synthesizer with a flexible modulation system."
+  homepage "https://plugins4free.com/plugin/3478"
+  depends_on :macos
+  pkg "Helm.pkg"
+end
